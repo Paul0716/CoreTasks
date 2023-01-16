3a098c24dd0ba6c8f7b67b1a4a6162ba06e0257b
Paul Ku <justlove0714@hotmail.com>
2023-01-16 08:58:08 +0800

    WIP

--- grapejs/webpack.config.js
+++ grapejs/webpack.config.js
@@ -1,7 +1,30 @@
+var webpack = require('webpack');
+
+const path = require('path');
+
 module.exports = {
-    entry: './src/index.js',
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'bundle.js',
-    },
-  };
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ]
+  },
+  
+};
